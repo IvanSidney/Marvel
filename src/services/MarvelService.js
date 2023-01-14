8fc8090376c665a4e299c28f853506bf7df93442
IvanSidney <ivansidney7@gmail.com>
2023-01-14 19:20:30 +0200

    new 14/01 API

---
+++ src/services/MarvelService.js
@@ -0,0 +1,25 @@
+class MarvelService {
+
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=92af49ccc0fbd9cc4f76d67b2adf13d0';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=310&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
+export default MarvelService;
